f69e15d9af149a3f7282cb7a84a5063e2edb9f5b
mryash08 <140238011+mryash08@users.noreply.github.com>
2023-08-08 19:29:14 +0530

    Time: 6 ms (37.17%), Space: 40.9 MB (45.84%) - LeetHub

--- 0228-summary-ranges/0228-summary-ranges.java
+++ 0228-summary-ranges/0228-summary-ranges.java
@@ -1,40 +1,32 @@
 class Solution {
     public List<String> summaryRanges(int[] nums) {
         
-        ArrayList<String> list = new ArrayList<>();
+        List<String> list = new ArrayList<>();
         
-       int start = 0; 
-        
-        if(nums.length == 1){
-            list.add(nums[start] + "");
+        if(nums.length < 1){
             return list;
-        }else if(nums.length == 0){
-               return list;
         }
-        
-        for(int i=0; i<nums.length; i++){
-            
-           
-           if(i < nums.length-1 && nums[i]+1 != nums[i+1]){
-               if(nums[start] == nums[i]){
-                    list.add(nums[start] + "");
-               }else{
-               list.add(nums[start] + "->" + nums[i]);
-               }
-               start = i+1;
-               continue;
-           } else if(i == nums.length-1 && nums[i-1] == nums[i]-1){
-                if(nums[start] == nums[i]){
-                    list.add(nums[start] + "");
-               }else{
-               list.add(nums[start] + "->" + nums[i]);
-               }
-               start = i+1;
-               continue;
-           } else if(start == nums.length-1){
-                list.add(nums[start]+"");
-           }
+
+        int start = 0;
+        for(int i=0; i< nums.length-1; i++){
+            if(nums[i] != (nums[i+1] - 1)){
+                if(i == start){
+                    list.add("" + nums[i]);
+                    start = i+1;
+                }else {
+                    list.add(nums[start] + "->" + nums[i]);
+                    start = i+1;
+
+                }
+            }
         }
-        return list;
+
+            if(nums.length-1 == start){
+                list.add("" + nums[nums.length-1]);
+            }else {
+                list.add(nums[start] + "->" + nums[nums.length-1]);
+            }
+
+            return list;
     }
 }
