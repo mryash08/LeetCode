16a3bf1222cc955eb03628ebeecf8c4fc8f2e044
mryash08 <140238011+mryash08@users.noreply.github.com>
2023-08-24 22:26:01 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (76.39%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.java
+++ 206-reverse-linked-list/reverse-linked-list.java
@@ -9,17 +9,34 @@
  * }
  */
 class Solution {
-    public ListNode reverseList(ListNode head) {
-        if(head == null || head.next == null){
-            return head;
+    // public ListNode reverseList(ListNode head) {
+    //     if(head == null || head.next == null){
+    //         return head;
+    //     }
+
+    //      ListNode res = reverseList(head.next);
+    //      head.next.next = head;
+    //      head.next = null;
+
+
+    //      return res;
+    // }
+
+      public ListNode reverseList(ListNode head) {
+        
+        ListNode curr = head;
+        ListNode prev = null;
+        ListNode upcoming = null;
+
+        while(curr != null){
+             upcoming = curr.next;
+             curr.next = prev;
+             prev = curr;
+             curr = upcoming;
         }
 
-         ListNode res = reverseList(head.next);
-         head.next.next = head;
-         head.next = null;
 
-
-         return res;
+         return prev;
     }
 
 
