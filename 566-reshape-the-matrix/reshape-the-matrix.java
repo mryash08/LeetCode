10c705176d9c42ce3ae3cc1fc336a31cd376161f
mryash08 <140238011+mryash08@users.noreply.github.com>
2023-08-24 17:12:32 +0530

    Time: 1 ms (46.76%) | Memory: 45 MB (7.55%) - LeetSync

--- 566-reshape-the-matrix/reshape-the-matrix.java
+++ 566-reshape-the-matrix/reshape-the-matrix.java
@@ -8,8 +8,7 @@ class Solution {
         int r1 = 0; int c1 =0;
         for(int i=0; i<mat.length; i++){
             for(int j=0; j<mat[i].length; j++){
-                  ans[r1][c1] = mat[i][j];
-                  c1++;
+                  ans[r1][c1++] = mat[i][j];
                   if(c1 > c-1){
                       c1 = 0;
                       r1++;
