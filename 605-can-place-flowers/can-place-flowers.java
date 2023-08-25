f5734ed0af63b4c82b5b4fc58c3c1e2535c470c5
mryash08 <140238011+mryash08@users.noreply.github.com>
2023-08-25 17:41:38 +0530

    Time: 1 ms (98.44%) | Memory: 44.1 MB (64.01%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.java
+++ 605-can-place-flowers/can-place-flowers.java
@@ -1,42 +1,31 @@
 class Solution {
     public boolean canPlaceFlowers(int[] flowerbed, int n) {
-        int countZero = 0;
-   if(n == 0){
-                    return true;
-                }
-      
-        if(flowerbed[0] == 0){
-            countZero++;
-        }
+        if(n == 0) return true;
+
+        if(flowerbed.length == 1 && flowerbed[0] == 0 && n != 0) return true;
+        
+
         for(int i=0; i<flowerbed.length; i++){
 
-            if(countZero == 0 && flowerbed[i] == 0){
-                countZero++;
-            }else if(i != flowerbed.length-1 && countZero == 1 && flowerbed[i] == 0 && flowerbed[i+1] == 0){
+           if(flowerbed[i] == 0){
+                 if(i==0 && flowerbed[i+1] == 0){
+                 n--;
                 flowerbed[i] = 1;
-                n--;
-                  if(n == 0){
-                    return true;
-                }
-                countZero = 0;
-            }else{
-                countZero = 0;
+            }else if (i== flowerbed.length-1 && flowerbed[i-1] == 0){
+                    n--;
+                flowerbed[i] = 1;
+            }else if(i != 0 && i!= flowerbed.length-1&& flowerbed[i-1] == 0 && flowerbed[i+1] == 0){
+                  n--;
+                flowerbed[i] = 1;
             }
-            
+           }
 
-
+           if(n==0){
+               return true;
+           }
         }
 
-        if(flowerbed.length > 1 && flowerbed[flowerbed.length-1] == 0 && flowerbed[flowerbed.length-2] == 0 || flowerbed.length == 1 && flowerbed[0] == 0){
-            n--;
-        }
+        return false;
        
-
-
-        if(n == 0){
-            return true;
-        }else{
-            return false;
-        }
     }
 }
