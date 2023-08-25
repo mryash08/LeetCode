abe26ac0ed9f016da7c8b5e00490b96518357c79
mryash08 <140238011+mryash08@users.noreply.github.com>
2023-08-25 17:48:33 +0530

    Time: 1 ms (98.44%) | Memory: 44 MB (78.04%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.java
+++ 605-can-place-flowers/can-place-flowers.java
@@ -3,25 +3,19 @@ class Solution {
         if(n == 0) return true;
        if(flowerbed.length == 1 && flowerbed[0] == 0 && n != 0) return true;
         
-
         for(int i=0; i<flowerbed.length; i++){
-
-             if(flowerbed[i] == 0){
-                if( i==0 && flowerbed[i+1] == 0 ||  i== flowerbed.length-1 && flowerbed[i-1] == 0 ||  i != 0 && i!= flowerbed.length-1&& flowerbed[i-1] == 0 &&                   flowerbed[i+1] == 0){
-                 
+              if(flowerbed[i] == 0){
+                if( i==0 && flowerbed[i+1] == 0 ||  i== flowerbed.length-1 && 
+                flowerbed [i-1] == 0 ||  i != 0 && i!= flowerbed.length-1&& 
+                flowerbed[i-1] == 0 && flowerbed[i+1] == 0){
                  n--;
                 flowerbed[i] = 1;
-            
                }
              }
-          
-
            if(n==0){
                return true;
            }
         }
-
         return false;
-       
     }
 }
