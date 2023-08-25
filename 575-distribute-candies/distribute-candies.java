f58391cf35e010992dcafc15e7eaaff2140ec9d5
mryash08 <140238011+mryash08@users.noreply.github.com>
2023-08-25 09:54:55 +0530

    Time: 29 ms (83.74%) | Memory: 44.7 MB (67.20%) - LeetSync

---
+++ 575-distribute-candies/distribute-candies.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int distributeCandies(int[] candyType) {
+
+
+      HashSet<Integer> set = new HashSet();
+      for(int num : candyType){
+          set.add(num);
+      }
+
+      return Math.min(candyType.length/2,set.size());
+
+       
+    }
+}
