9129c9e4286e4b49f4360f09376199a6020f0d2f
mryash08 <140238011+mryash08@users.noreply.github.com>
2023-08-29 14:26:56 +0530

    Time: 13 ms (62.29%) | Memory: 44.3 MB (58.59%) - LeetSync

--- 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.java
+++ 2576-minimum-penalty-for-a-shop/minimum-penalty-for-a-shop.java
@@ -1,45 +1,27 @@
 class Solution {
     public static int bestClosingTime(String customers) {
         int y = 0;
-        int n = 0;
         for(int i=0; i<customers.length(); i++){
-            if(customers.charAt(i) == 'Y'){
-
-                y++;
-            }else{
-                n++;
-            }
+            if(customers.charAt(i) == 'Y')y++;
         }
-
-
-
         int ans = 0;
         int minPenlty = Integer.MAX_VALUE;
         int previousn = 0;
 
         for(int i=0; i<customers.length(); i++){
             int penlty = 0;
-            if(customers.charAt(i) == 'Y'){
-                penlty = y+previousn;
-                y--;
-            }else{
-                penlty = y+previousn;
-                previousn++;
-
-
-            }
+            penlty = y+previousn;
+            
+            if(customers.charAt(i) == 'Y') y--;
+            else previousn++;
 
             if(penlty < minPenlty){
                 ans = i;
                 minPenlty = penlty;
             }
         }
-
-        if(previousn < minPenlty){
-            return customers.length();
-        }
-
-
+        if(previousn < minPenlty) return customers.length();
+        
         return ans;
     }
 }
