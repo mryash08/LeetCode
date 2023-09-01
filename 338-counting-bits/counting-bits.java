1982e205748926467ac21998438cb0dd4784a36d
mryash08 <140238011+mryash08@users.noreply.github.com>
2023-09-01 09:32:30 +0530

    Time: 11 ms (13.02%) | Memory: 52.4 MB (5.59%) - LeetSync

--- 338-counting-bits/counting-bits.java
+++ 338-counting-bits/counting-bits.java
@@ -7,27 +7,22 @@ class Solution {
         for(int i=0; i<=n && i<=2; i++){
             if(i == 0){
                 map.put(0,0);
-              ans[0] = 0;
+                ans[0] = 0;
             }else{
-          map.put(i,1);
-
-
-          ans[i] = 1;
-
+                map.put(i,1);
+                ans[i] = 1;
             }
         }
         for(int i=3; i<=n; i++){
             int val  = 0;
              if(i%2 == 0){
                   val = map.get(i/2);
-                 map.put(i,val);
-             
+                  map.put(i,val);
              }else{
-                val = map.get(i/2) + 1;
+                 val = map.get(i/2) + 1;
                  map.put(i,val);
              }
-            
-    ans[i] = val;
+             ans[i] = val;
         }
         return ans;
     }
