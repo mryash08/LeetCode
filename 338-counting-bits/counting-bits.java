6bbd5372bc56a9f1bd878ba4a11989aa83087a69
mryash08 <140238011+mryash08@users.noreply.github.com>
2023-09-01 09:38:17 +0530

    Time: 2 ms (73.73%) | Memory: 45.8 MB (99.06%) - LeetSync

--- 338-counting-bits/counting-bits.java
+++ 338-counting-bits/counting-bits.java
@@ -1,29 +1,9 @@
 class Solution {
     public int[] countBits(int n) {
-
-        HashMap<Integer,Integer> map = new HashMap<>();
-        int[] ans = new int[n+1];
-
-        for(int i=0; i<=n && i<=2; i++){
-            if(i == 0){
-                map.put(0,0);
-                ans[0] = 0;
-            }else{
-                map.put(i,1);
-                ans[i] = 1;
-            }
-        }
-        for(int i=3; i<=n; i++){
-            int val  = 0;
-             if(i%2 == 0){
-                  val = map.get(i/2);
-                  map.put(i,val);
-             }else{
-                 val = map.get(i/2) + 1;
-                 map.put(i,val);
-             }
-             ans[i] = val;
-        }
-        return ans;
+       int[] ans = new int[n+1];
+       for(int i=0; i<=n; i++){
+          ans[i] = ans[i /2] + i % 2;
+       }
+       return ans;
     }
 }
