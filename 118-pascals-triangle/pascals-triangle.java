bdef4ca6d298179ab86d8090ee6b69668b0fa47f
mryash08 <140238011+mryash08@users.noreply.github.com>
2023-09-08 11:12:46 +0530

    Time: 1 ms (89.25%) | Memory: 40.9 MB (63.83%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.java
+++ 118-pascals-triangle/pascals-triangle.java
@@ -10,10 +10,11 @@ class Solution {
         row1.add(1);
         ans.add(row1);
         for(int i=1; i<numRows; i++){
+            List<Integer> prevRow = ans.get(i-1);
             List<Integer> row = new ArrayList<>();
             row.add(1);
             for(int j=1; j<i; j++){
-                row.add(ans.get(i-1).get(j) + ans.get(i-1).get(j-1));
+                row.add(prevRow.get(j) + prevRow.get(j-1));
             }
             row.add(1);
             ans.add(row);
