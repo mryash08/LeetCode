62a5d01ef6b3404849eec1b6ac79ecb76c284008
mryash08 <140238011+mryash08@users.noreply.github.com>
2023-09-14 21:28:45 +0530

    Time: 1 ms (96.54%) | Memory: 42.6 MB (99.42%) - LeetSync

---
+++ 983-validate-stack-sequences/validate-stack-sequences.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+
+       int j=0; int i=0;
+        while(j < popped.length-1){
+            if(pushed[i] == popped[j]){
+                pushed[i] = -1;
+                do{
+                    i--;
+                }while(i >= 0 && pushed[i] == -1);
+                j++;
+                if(i < 0){
+                    i = j;
+                }
+            }else{
+                do{
+                    i++;
+                } while(i < pushed.length-1 && pushed[i] == -1);
+            }
+            if(i > pushed.length-1){
+                return false;
+            }
+        }
+        return true;
+    }
+}
