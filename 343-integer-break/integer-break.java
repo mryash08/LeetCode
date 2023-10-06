fd69caacf04c407764677f83c9be507eb700673a
mryash08 <140238011+mryash08@users.noreply.github.com>
2023-10-06 11:13:48 +0530

    Time: 0 ms (100.00%) | Memory: 39 MB (74.61%) - LeetSync

--- 343-integer-break/integer-break.java
+++ 343-integer-break/integer-break.java
@@ -1,10 +1,9 @@
 class Solution {
     public int integerBreak(int n) {
-
-       
         int division = 2;
         int maxProduct = 1;
-        while(n/division >= 1){
+        if(n == 3) return 2;
+        while(n/division > 1){
             int sum = 0;
             int product = 1;
             for(int i=0; i<division-1; i++){
