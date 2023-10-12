ff77bf33f74ae9e59800d327c44ed092869dfda1
mryash08 <140238011+mryash08@users.noreply.github.com>
2023-10-12 11:56:08 +0530

    Time: 19 ms (37.12%) | Memory: 57 MB (25.88%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -7,12 +7,8 @@ class Solution {
             if(Math.abs(i-k) < 0){
                 map.remove(i-k);
             }
-            if(map.containsKey(nums[i])){
-                if(Math.abs(map.get(nums[i])-i) <= k ){
+            if(map.containsKey(nums[i]) && Math.abs(map.get(nums[i])-i) <= k ){
                     return true;
-                }else{
-                    map.put(nums[i],i);
-                }
             }else {
                 map.put(nums[i], i);
             }
