3b9819c14303c33ffc2bb32386cff50920a2789b
mryash08 <140238011+mryash08@users.noreply.github.com>
2023-10-12 11:49:28 +0530

    Time: 19 ms (37.12%) | Memory: 56.5 MB (50.93%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.java
+++ 219-contains-duplicate-ii/contains-duplicate-ii.java
@@ -4,6 +4,7 @@ class Solution {
        Map<Integer,Integer> map = new HashMap<>();
 
         for(int i=0; i< nums.length; i++){
+            
             if(map.containsKey(nums[i])){
                 if(Math.abs(map.get(nums[i])-i) <= k ){
                     return true;
