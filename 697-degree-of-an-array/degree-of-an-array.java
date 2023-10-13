cd85bbefafe82313065c7a495c304996e8d79924
mryash08 <140238011+mryash08@users.noreply.github.com>
2023-10-13 10:04:39 +0530

    Time: 15 ms (81.50%) | Memory: 47 MB (21.60%) - LeetSync

--- 697-degree-of-an-array/degree-of-an-array.java
+++ 697-degree-of-an-array/degree-of-an-array.java
@@ -3,27 +3,22 @@ class Solution {
         
         HashMap<Integer,int[]> map = new HashMap<>();
         int maxValue = -1;
-        int maxLength = Integer.MAX_VALUE;
-
+        int maxLength = 1;
         for(int i=0; i<nums.length; i++){
             if(map.containsKey(nums[i])){
                int temp[] =  map.get(nums[i]);
                temp[1] = i;
                temp[2]++;
-                map.put(nums[i],temp);
+               map.put(nums[i],temp);
+               int count = temp[2];
+               int diff = (temp[1] - temp[0]) + 1;
+                if(count > maxValue || count == maxValue && diff < maxLength){
+                   maxValue = count;
+                   maxLength = diff;
+                }
             }else{
                 int temp[] = {i , i , 1};
                 map.put(nums[i],temp);
-            }
-
-            int temp[] =  map.get(nums[i]);
-            int count = temp[2];
-            int diff = (temp[1] - temp[0]) + 1;
-            
-
-            if(count > maxValue || count == maxValue && diff < maxLength){
-                maxValue = count;
-                maxLength = diff;
             }
         }
         return maxLength;
