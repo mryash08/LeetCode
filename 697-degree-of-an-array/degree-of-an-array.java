cef6c3d6daa966da9d3e36a81bb88d348b030a7a
mryash08 <140238011+mryash08@users.noreply.github.com>
2023-10-13 09:57:25 +0530

    Time: 19 ms (74.51%) | Memory: 46 MB (53.75%) - LeetSync

---
+++ 697-degree-of-an-array/degree-of-an-array.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int findShortestSubArray(int[] nums) {
+        
+        HashMap<Integer,int[]> map = new HashMap<>();
+        int maxValue = -1;
+        int maxLength = Integer.MAX_VALUE;
+
+        for(int i=0; i<nums.length; i++){
+            if(map.containsKey(nums[i])){
+               int temp[] =  map.get(nums[i]);
+               temp[1] = i;
+               temp[2]++;
+                map.put(nums[i],temp);
+            }else{
+                int temp[] = {i , -1 , 1};
+                map.put(nums[i],temp);
+            }
+
+            int temp[] =  map.get(nums[i]);
+            int count = temp[2];
+            int diff;
+            if(temp[1] == -1){
+                 diff = 1;
+            }else{
+                 diff = (temp[1] - temp[0]) + 1;
+            }
+
+            if(count > maxValue || count == maxValue && diff < maxLength){
+                maxValue = count;
+                maxLength = diff;
+            }
+        }
+        return maxLength;
+    }
+}
