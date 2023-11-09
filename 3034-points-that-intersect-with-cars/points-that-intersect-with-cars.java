6eb542f2e236ce46ae3aab2128542a1842643fe1
mryash08 <140238011+mryash08@users.noreply.github.com>
2023-11-09 15:26:08 +0530

    Time: 6 ms (17.49%) | Memory: 43.3 MB (45.34%) - LeetSync

--- 3034-points-that-intersect-with-cars/points-that-intersect-with-cars.java
+++ 3034-points-that-intersect-with-cars/points-that-intersect-with-cars.java
@@ -1,7 +1,6 @@
 class Solution {
     public int numberOfPoints(List<List<Integer>> nums) {
         HashSet<Integer> set = new HashSet<>();
-
         for(int i=0; i<nums.size(); i++){
             for(int j=nums.get(i).get(0); j<=nums.get(i).get(1); j++){
                 set.add(j);
