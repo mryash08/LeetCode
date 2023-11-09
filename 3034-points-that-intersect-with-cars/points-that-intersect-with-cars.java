ff91f00f351f3bd3ebd68402e452f7493d803ecb
mryash08 <140238011+mryash08@users.noreply.github.com>
2023-11-09 15:25:59 +0530

    Time: 5 ms (47.82%) | Memory: 43.2 MB (55.72%) - LeetSync

--- 3034-points-that-intersect-with-cars/points-that-intersect-with-cars.java
+++ 3034-points-that-intersect-with-cars/points-that-intersect-with-cars.java
@@ -1,22 +1,12 @@
 class Solution {
     public int numberOfPoints(List<List<Integer>> nums) {
-        Collections.sort(nums, new Comparator<List<Integer>>() {
-            public int compare(List<Integer> o1, List<Integer> o2) {
-                return o1.get(0).compareTo(o2.get(0));
-            }
-        });
-        int ans = 0;
-        int max = nums.get(0).get(1);
-         ans = ans + (nums.get(0).get(1) - nums.get(0).get(0)) + 1;
-        for(int i=1; i<nums.size(); i++){
-            if(nums.get(i).get(0) <= max && nums.get(i).get(1) > max){
-                ans += nums.get(i).get(1) - max;
-                max = nums.get(i).get(1);
-            }else if(nums.get(i).get(0) > max){
-                ans += nums.get(i).get(1) - nums.get(i).get(0) +1;
-                max = nums.get(i).get(1);
+        HashSet<Integer> set = new HashSet<>();
+
+        for(int i=0; i<nums.size(); i++){
+            for(int j=nums.get(i).get(0); j<=nums.get(i).get(1); j++){
+                set.add(j);
             }
         }
-        return ans;
+        return set.size();
     }
 }
