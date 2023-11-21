9693e347fd949a1b6aaf963e0fc212bcd9057623
mryash08 <140238011+mryash08@users.noreply.github.com>
2023-11-21 10:07:52 +0530

    Time: 3 ms (85.89%) | Memory: 86.3 MB (85.28%) - LeetSync

--- 1803-average-waiting-time/average-waiting-time.java
+++ 1803-average-waiting-time/average-waiting-time.java
@@ -4,12 +4,10 @@ class Solution {
         double waitTime = 0.0;
         for(int i=0; i<customers.length; i++){
              if(lastTime < customers[i][0]){
-                 lastTime = customers[i][0] + customers[i][1];
-                 waitTime += customers[i][1];
-              }else{
+                 lastTime = customers[i][0];
+              }
                  lastTime += customers[i][1];
                  waitTime += lastTime - customers[i][0];
-              }
         }
         return waitTime/customers.length;
     }
