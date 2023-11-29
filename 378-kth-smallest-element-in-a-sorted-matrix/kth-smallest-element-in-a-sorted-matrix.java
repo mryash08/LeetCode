01680c51b36c66eca48edc96db40808ceabdd2b5
mryash08 <140238011+mryash08@users.noreply.github.com>
2023-11-29 16:06:58 +0530

    Time: 0 ms (100.00%) | Memory: 47.9 MB (18.90%) - LeetSync

--- 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
+++ 378-kth-smallest-element-in-a-sorted-matrix/kth-smallest-element-in-a-sorted-matrix.java
@@ -1,13 +1,30 @@
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-        int[] freq = new int[matrix.length*matrix.length];
-        int idx = 0;
-        for(int i=0; i<matrix.length; i++){
-            for(int j=0; j<matrix.length; j++){
-                freq[idx++] = matrix[i][j];
-            }
+        int start = matrix[0][0];
+        int end = matrix[matrix.length-1][matrix.length-1];
+        int mid = 0;
+        while(start < end){
+              mid = start + (end-start)/2;
+              int count = posEle(matrix,mid);
+              if(count < k){
+                  start = mid+1;
+              }else{
+                  end = mid;
+              }
         }
-        Arrays.sort(freq);
-        return freq[k-1];
+        return start;
     }
+    public int posEle(int[][] mat ,int mid){
+            int i=mat.length-1; int j=0; int count = 0;
+            while(i>=0 && j<mat.length){
+                if(mid < mat[i][j]){
+                     i--;
+                }else{
+                    j++;
+                    count += i+1; 
+                }
+           }
+       return count;
+    }
+    
 }
