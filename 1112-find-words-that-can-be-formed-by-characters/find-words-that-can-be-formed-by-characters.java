316ff7b4250f166aa56939ebbdead32fa85d5ac3
mryash08 <140238011+mryash08@users.noreply.github.com>
2023-12-02 21:37:54 +0530

    Time: 2 ms (100.00%) | Memory: 44.5 MB (21.73%) - LeetSync

--- 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.java
+++ 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.java
@@ -1,28 +1,22 @@
 class Solution {
     public int countCharacters(String[] words, String chars) {
         int[] counts = new int[26];
-        for(int i=0;i<chars.length();i++)
-        {
+        for(int i=0;i<chars.length();i++){
             counts[chars.charAt(i)-'a']++;
         }
         int res =0 ;
-        for(String s : words)
-        {
-            if(canForm(s,counts))
-                res+=s.length();
+        for(String s : words){
+            if(isValid(s,counts)) res+=s.length();
         }
         return res;
     }
 
-    boolean canForm(String word, int[] counts)
-    {
-        int[] c = new int[26];
-        for(int i =0;i<word.length();i++)
-        {
+    boolean isValid(String word, int[] counts){
+        int[] count1 = new int[26];
+        for(int i =0;i<word.length();i++){
             int x = word.charAt(i)-'a';
-            c[x]++;
-            if(c[x] > counts[x])
-                return false;
+            count1[x]++;
+            if(count1[x] > counts[x]) return false;
         }
         return true;
     }
