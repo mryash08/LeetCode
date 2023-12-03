6250b025b9a3f83fda7ea0bddde070e89e86fba8
mryash08 <140238011+mryash08@users.noreply.github.com>
2023-12-03 17:00:12 +0530

    Time: 0 ms (100.00%) | Memory: 41.1 MB (12.50%) - LeetSync

--- 537-complex-number-multiplication/complex-number-multiplication.java
+++ 537-complex-number-multiplication/complex-number-multiplication.java
@@ -1,88 +1,46 @@
 class Solution {
     public String complexNumberMultiply(String num1, String num2) {
-        char s1 = '+';
-        char s2 = '+';
-        int i=0; int j=0;
-        if(num1.charAt(0) == '-'){
-            s1 = '-';
-            i++;
-        }
-        if(num2.charAt(0) == '-'){
-            s2 = '-';
-            j++;
-        }
-        int n1 = 0; int multi = 10;
-        while(num1.charAt(i) >= 48 && num1.charAt(i) <= 57){
-            n1 = n1*multi + (num1.charAt(i++)-'0');
-        }
-        int n2 = 0;  multi = 10;
-        while(num2.charAt(j) >= 48 && num2.charAt(j) <= 57){
-            n2 = n2*multi + (num2.charAt(j++)-'0');
-        }
-
-        String sign1 = ""+num1.charAt(i++);
-        while(num1.charAt(i) == '+' || num1.charAt(i) == '-'){
-            sign1 += num1.charAt(i++);
-        }
-        String sign2 = ""+num2.charAt(j++);
-        while(num2.charAt(j) == '+' || num2.charAt(j) == '-'){
-            sign2 += num2.charAt(j++);
-        }
-        int f1 = 0;  multi = 10;
-        while(num1.charAt(i) >= 48 && num1.charAt(i) <= 57){
-            f1 = f1*multi + (num1.charAt(i++)-'0');
-        }
-        int f2 = 0;  multi = 10;
-        while(num2.charAt(j) >= 48 && num2.charAt(j) <= 57){
-            f2 = f2*multi + (num2.charAt(j++)-'0');
-        }
-        int first = 0;
-        if(s1 == '-' && s2 == '-'){
-            first = -n1 * -n2;
-        }else if(s1 == '-'){
-            first = -n1 * n2;
-        }else if(s2 == '-'){
-            first = n1 * -n2;
-        }else{
-            first = n1 * n2;
-        }
-        int second = 0;
-        int third = 0;
-       if(sign1.equals("+-") && sign2.equals("+-")){
-            second = -f1 * -f2;
-        }else if(sign1.equals("+-")){
-            second = -f1 * f2;
-        }else if(sign2.equals("+-")){
-            second = f1 * -f2;
-        }else{
-            second = f1 * f2;
-        }
-        int t1 = 0; int t2 = 0;
-        if(sign1.equals("+-") && s2 == '-'){
-            t1 = -f1 * -n2;
-        }else if(sign1.equals("+-")){
-            t1 = -f1 * n2;
-        }else if(s2 == '-'){
-            t1 = f1 * -n2;
-        }else{
-            t1 = f1 * n2;
-        }
-        if(sign2.equals("+-") && s1 == '-'){
-            t2 = -f2 * -n1;
-        }else if(sign2.equals("+-")){
-            t2 = -f2 * n1;
-        }else if(s1 == '-'){
-            t2 = f2 * -n1;
-        }else{
-            t2 = f2 * n1;
-        }
-        int fFirst = first + (second * -1);
-        int fsecond = t1 + t2;
-        StringBuilder ans = new StringBuilder();
-        ans.append(fFirst);
-        ans.append('+');
-        ans.append(fsecond);
-        ans.append("i");
-        return ans.toString();
-    }
+            
+            int[] dm1=new int[2];
+    int[] dm2=new int[2];
+    StringBuilder s=new StringBuilder();
+    dm1=checkInt(num1);
+    dm2=checkInt(num2);
+    int int1=dm1[0];
+    int int2=dm2[0];
+    int img1=checkImg(num1,dm1[1]);
+    int img2=checkImg(num2,dm2[1]);
+    int prodin=int1*int2-img1*img2;
+    int prodim=int1*img2+int2*img1;
+    s.append(prodin);
+	s.append("+");
+    s.append(prodim);
+    s.append("i");
+    return s.toString(); 
 }
+ public int[] checkInt(String num){
+     int[] intp=new int[2];
+     String s=new String();
+    for(int i=0;i<=4;i++){
+        if(num.charAt(i)=='-' && i==0){
+            continue;
+        }
+        if(num.charAt(i)=='+'){
+            s=num.substring(0,i);
+            intp[0]=Integer.parseInt(s);
+            intp[1]=i;
+            break;
+        }
+    }
+     return intp;
+ }
+ public int checkImg(String num,int imin){
+     int img=0;
+     String s=new String();
+     s=num.substring(imin+1,num.length()-1);
+     img=Integer.parseInt(s);
+    
+     return img;
+ }
+
+    }
