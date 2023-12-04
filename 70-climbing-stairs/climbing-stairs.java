b78076996e80363fc84ecde15cea759b0ab0738b
mryash08 <140238011+mryash08@users.noreply.github.com>
2023-12-04 16:14:25 +0530

    Time: 0 ms (100.00%) | Memory: 39.3 MB (33.66%) - LeetSync

---
+++ 70-climbing-stairs/climbing-stairs.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int climbStairs(int n) {
+        int a = 1;
+        int b = 1;
+        int i=1;
+        while(i != n){
+            int temp = a + b;
+            a = b;
+            b = temp;
+            i++;
+        }
+        return b;
+    }
+}
