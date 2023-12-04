0686854ed0d2fb56240cdf08125ac22584a59b71
mryash08 <140238011+mryash08@users.noreply.github.com>
2023-12-04 08:49:12 +0530

    Time: 6 ms (30.30%) | Memory: 41.8 MB (22.10%) - LeetSync

--- 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.java
+++ 2346-largest-3-same-digit-number-in-string/largest-3-same-digit-number-in-string.java
@@ -3,8 +3,9 @@ class Solution {
        char[] str = num.toCharArray();
         String ans = "";
         for(int i=9; i>=0; i--){
-            if(num.contains(i+""+i+""+i)){
-                return i+""+i+""+i;
+            String s = String.format("%d%d%d",i,i,i);
+            if(num.contains(s)){
+                return s;
             }
         }
         return "";
