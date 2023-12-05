97d73f31d55fbb114ae6ca935683dee59479d4c7
mryash08 <140238011+mryash08@users.noreply.github.com>
2023-12-05 15:49:48 +0530

    Time: 7 ms (98.87%) | Memory: 44.8 MB (21.47%) - LeetSync

--- 2187-vowels-of-all-substrings/vowels-of-all-substrings.java
+++ 2187-vowels-of-all-substrings/vowels-of-all-substrings.java
@@ -2,9 +2,10 @@ class Solution {
     public long countVowels(String word) {
          long ans = 0;
          byte[] arr = word.getBytes();
-         for(int i=0; i<(long)word.length(); i++){
+         long n = arr.length;
+         for(int i=0; i<n; i++){
               if(arr[i] == 'a' || arr[i] == 'e'|| arr[i] == 'i'||arr[i] == 'o'||arr[i] == 'u'){
-                  ans += (i+1) * ((long)word.length() - i);
+                  ans += (i+1) * (n - i);
               }
          }
          return ans;
