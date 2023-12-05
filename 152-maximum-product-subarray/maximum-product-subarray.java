953b293a4358467c1bd04413ef515f449c8967c8
mryash08 <140238011+mryash08@users.noreply.github.com>
2023-12-05 17:33:50 +0530

    Time: 1 ms (88.39%) | Memory: 43.5 MB (36.21%) - LeetSync

--- 152-maximum-product-subarray/maximum-product-subarray.java
+++ 152-maximum-product-subarray/maximum-product-subarray.java
@@ -1,56 +1,22 @@
 class Solution {
-    public int maxProduct(int[] nums) {
-        if(nums.length == 1) return nums[0];
-        int[] temp = {-3,2,3,0,-1};
-        int[] temp2 = {-3,-1,3,5,-6,-6,-1,6,-3,-5,1,0,-6,-5,0,-2,6,1,0,5};
-        int[] temp3 = {1,-2,0,1,-4,1,1,5,4,-1,6,4,1,-5,0,-1,-5,1,-6,-4};
-        int[] temp4 = {0,-1,4,-4,5,-2,-1,-1,-2,-3,0,-3,0,1,-1,-4,4,6,2,3,0,-5,2,1,-4,-2,-1,3,-4,-6,0,2,2,-1,-5,1,1,5,-6,2,1,-3,-6,-6,-3,4,0,-2,0,2};
-        if(Arrays.equals(nums,temp)) return 6;
-        if(Arrays.equals(nums,temp2)) return 48600;
-        if(Arrays.equals(nums,temp3)) return 2400;
-        if(Arrays.equals(nums,temp4)) return 388800;
-         int count = 0;
-         int zeroCount = 0;
-         if(nums[0] < 0) count++;
-         for(int i=1; i<nums.length; i++){
-             if(nums[i] < 0) count++;
-             if(nums[i-1] == 0){
-                 zeroCount++;
-             }else{
-                nums[i] *= nums[i-1];
-             }
-         }
-         if(nums[nums.length-1] == 0) zeroCount++;
-         if(count % 2 == 0 && zeroCount == 0) return nums[nums.length-1];
-         int ans = 0;
-         int j = nums.length-1;
-         while(nums[j] == 0){
-               j--;
-               zeroCount--;
-         }
-         for(int i=0; i<j; i++){
-             if(i == 0 && nums[i] < 0 && zeroCount == 0){
-                  ans = nums[j]/nums[0];
-             }else if(i == 0 && nums[i] == 0){
-                    ans = nums[j];
-                    zeroCount--;
-             }else if(i != 0 && nums[i] == 0){
-                  ans = Math.max(ans,nums[i-1]);
-                  ans = Math.max(ans,nums[j]);
-                  zeroCount--;
-             }else if(i != 0 && nums[i-1] != 0 && nums[i]/nums[i-1] < 0){
-                ans = Math.max(ans,nums[i-1]);
-                  ans = Math.max(ans,nums[j]/nums[i]);
-             }else if(i != 0 && nums[i-1] == 0 && nums[i] < 0 && zeroCount == 0){
-                 ans = Math.max(ans,nums[j]/nums[i]);
-             }else if(i != 0 && nums[i-1] != 0 && nums[i]/nums[i-1] > 0){
-                 ans = Math.max(ans,nums[i]/nums[i-1]);
-             }
-         }
-         if(j > 0){
-             ans = Math.max(ans,nums[j-1]);
-         }
-         ans = Math.max(ans,nums[j]);
-         return ans;
+     public int maxProduct(int[] nums) {
+        int mpp=nums[0];
+        int mnp=nums[0]; 
+        int omax=nums[0]; 
+
+        for(int i=1;i<nums.length;i++){
+            int val=nums[i];
+            if(val<0){
+                int temp1=mnp;
+                int temp2=mpp;
+              mpp=Math.max(val,temp1*val);
+              mnp=Math.min(val,temp2*val);  
+            }else{
+               mpp=Math.max(val,val*mpp);
+               mnp=Math.min(val,val*mnp); 
+            }
+            omax=Math.max(mpp,omax);
+        }
+        return omax;
     }
 }
