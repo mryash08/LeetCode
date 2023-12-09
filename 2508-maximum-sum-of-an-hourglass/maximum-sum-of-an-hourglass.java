1a036f63aac113868d289d8dcbcb927a5e464acf
mryash08 <140238011+mryash08@users.noreply.github.com>
2023-12-09 10:59:40 +0530

    Time: 2 ms (100.00%) | Memory: 46.3 MB (7.10%) - LeetSync

--- 2508-maximum-sum-of-an-hourglass/maximum-sum-of-an-hourglass.java
+++ 2508-maximum-sum-of-an-hourglass/maximum-sum-of-an-hourglass.java
@@ -1,25 +1,22 @@
 class Solution {
     public int maxSum(int[][] grid) {
-        for(int i=0; i<grid.length; i++){
-            for(int j=1; j<grid[0].length; j++){
-                grid[i][j] += grid[i][j-1];
+        int max = 0;
+        int n = grid.length;
+        int m = grid[0].length;
+        for(int i = 0; i < n-2; i++) {
+            for(int j = 0; j < m-2; j++) {
+                int sum = 0;
+                for(int z = j; z < j+3; z++) {
+                    sum += grid[i][z];
+                }
+                sum += grid[i+1][j+1];
+                 for(int z = j; z < j+3; z++) {
+                    sum += grid[i+2][z];
+                }
+                max = Math.max(sum, max);
+
             }
         }
-        int ans = 0;
-        for(int i=2; i<grid.length; i++){
-            for(int j=2; j<grid[0].length; j++){
-                 int sum = 0;
-                  if(j == 2){
-                        sum = grid[i][j] + grid[i-2][j];
-                        sum += grid[i-1][j-1] - grid[i-1][j-2];
-                  }else{
-                       sum += (grid[i][j] - grid[i][j-3]) ;
-                       sum += (grid[i-2][j] - grid[i-2][j-3]);
-                       sum += (grid[i-1][j-1] - grid[i-1][j-2]);
-                  }
-                  ans = Math.max(sum,ans);
-            }
-        }
-        return ans;
+        return max;
     }
 }
