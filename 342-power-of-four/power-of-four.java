3c790394af210cf54fe2051d72982c87bc95c31f
mryash08 <140238011+mryash08@users.noreply.github.com>
2023-12-11 14:02:47 +0530

    Time: 0 ms (100.00%) | Memory: 39.8 MB (25.28%) - LeetSync

--- 342-power-of-four/power-of-four.java
+++ 342-power-of-four/power-of-four.java
@@ -1,9 +1,23 @@
 class Solution {
     public boolean isPowerOfFour(int n) {
+        // using recursion
+        // if(n == 1 || n == 4) return true;
+        // if(n % 4 != 0 || n == 0) return false;
+        // return isPowerOfFour(n/4);
+
+        //    using bit manipulation
+    //     if(n == 0) return false;
+    //     for(int i=0; (1 << i) <= n && i <= 32; i+=2){
+    //           if((1 << i) == n) return true;
+    //     }
+    //    return false;
+    
+    //   using math
         if(n == 0) return false;
-        for(int i=0; (1 << i) <= n && i <= 32; i+=2){
-              if((1 << i) == n) return true;
-        }
-       return false;
+        while(n != 1){
+            if(n % 4 != 0) return false;
+            n /= 4;       
+       }
+       return true;
     }
 }
