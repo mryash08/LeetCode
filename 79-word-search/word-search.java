4481b312882e39482b16efb0908f11c75925a708
mryash08 <140238011+mryash08@users.noreply.github.com>
2023-12-23 11:40:17 +0530

    Time: 180 ms (33.90%) | Memory: 41.7 MB (25.32%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,27 +1,28 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        StringBuilder str = new StringBuilder(word);
+        ArrayList<Character> str = new ArrayList<>();
+        for(int i=0; i<word.length(); i++){
+            str.add(word.charAt(i));
+        }
         for(int i=0; i<board.length; i++){
             for(int j=0; j<board[0].length; j++){
-                if(ifExist(board,str,new StringBuilder(),new boolean[board.length][board[0].length],i,j)) return true;
+                if(ifExist(board,str,new ArrayList<>(),new boolean[board.length][board[0].length],i,j)) return true;
             }
         }
         return false;
     }
-    public boolean ifExist(char[][] board,StringBuilder word,StringBuilder ans,boolean[][] check,int x,int y){
-        if(ans.length() == word.length()) return true;
+    public boolean ifExist(char[][] board,ArrayList<Character> word,ArrayList<Character> ans,boolean[][] check,int x,int y){
+        if(ans.size() == word.size()) return true;
         if(x < 0 || y < 0 || x >= board.length || y >= board[0].length) return false;
-        if(word.charAt(ans.length()) != board[x][y]) return false;
+        if(word.get(ans.size()) != board[x][y]) return false;
         if(check[x][y]) return false;
         else check[x][y] = true;
-        if(ifExist(board,word,ans.append(board[x][y]),check,x-1,y)) return true;
-        ans.deleteCharAt(ans.length()-1);
-        if(ifExist(board,word,ans.append(board[x][y]),check,x,y+1)) return true;
-        ans.deleteCharAt(ans.length()-1);
-        if(ifExist(board,word,ans.append(board[x][y]),check,x+1,y)) return true;
-        ans.deleteCharAt(ans.length()-1);
-        if (ifExist(board,word,ans.append(board[x][y]),check,x,y-1)) return true;
-        ans.deleteCharAt(ans.length()-1);
+        ans.add(board[x][y]);
+        if(ifExist(board,word,ans,check,x-1,y)) return true;
+        if(ifExist(board,word,ans,check,x,y+1)) return true;
+        if(ifExist(board,word,ans,check,x+1,y)) return true;
+        if (ifExist(board,word,ans,check,x,y-1)) return true;
+        ans.remove(ans.size()-1);
         check[x][y] = false;
         return false;
     }
