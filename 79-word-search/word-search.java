88e69e7521b2fd4afaec381ba6db30e1eb38615d
mryash08 <140238011+mryash08@users.noreply.github.com>
2023-12-23 12:07:54 +0530

    Time: 127 ms (86.66%) | Memory: 41.4 MB (25.84%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,20 +1,16 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        ArrayList<Character> str = new ArrayList<>();
-        for(int i=0; i<word.length(); i++){
-            str.add(word.charAt(i));
-        }
         for(int i=0; i<board.length; i++){
             for(int j=0; j<board[0].length; j++){
-                if(ifExist(board,str,0,i,j)) return true;
+                if(ifExist(board,word,0,i,j)) return true;
             }
         }
         return false;
     }
-    public boolean ifExist(char[][] board,ArrayList<Character> word,int count,int x,int y){
-        if(count == word.size()) return true;
+    public boolean ifExist(char[][] board,String word,int count,int x,int y){
+        if(count == word.length()) return true;
         if(x < 0 || y < 0 || x >= board.length || y >= board[0].length) return false;
-        if(word.get(count) != board[x][y]) return false;
+        if(word.charAt(count) != board[x][y]) return false;
         int temp = board[x][y];
         board[x][y] = ' ';
         count++;
