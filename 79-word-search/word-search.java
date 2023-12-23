ce419f4f9ce9b1aabe0b6e965d6030ecd054f944
mryash08 <140238011+mryash08@users.noreply.github.com>
2023-12-23 11:49:46 +0530

    Time: 123 ms (88.21%) | Memory: 41.4 MB (26.45%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -6,23 +6,23 @@ class Solution {
         }
         for(int i=0; i<board.length; i++){
             for(int j=0; j<board[0].length; j++){
-                if(ifExist(board,str,new ArrayList<>(),i,j)) return true;
+                if(ifExist(board,str,0,i,j)) return true;
             }
         }
         return false;
     }
-    public boolean ifExist(char[][] board,ArrayList<Character> word,ArrayList<Character> ans,int x,int y){
-        if(ans.size() == word.size()) return true;
+    public boolean ifExist(char[][] board,ArrayList<Character> word,int count,int x,int y){
+        if(count == word.size()) return true;
         if(x < 0 || y < 0 || x >= board.length || y >= board[0].length) return false;
-        if(word.get(ans.size()) != board[x][y]) return false;
+        if(word.get(count) != board[x][y]) return false;
         int temp = board[x][y];
         board[x][y] = ' ';
-        ans.add(board[x][y]);
-        if(ifExist(board,word,ans,x-1,y)) return true;
-        if(ifExist(board,word,ans,x,y+1)) return true;
-        if(ifExist(board,word,ans,x+1,y)) return true;
-        if (ifExist(board,word,ans,x,y-1)) return true;
-        ans.remove(ans.size()-1);
+        count++;
+        if(ifExist(board,word,count,x-1,y)) return true;
+        if(ifExist(board,word,count,x,y+1)) return true;
+        if(ifExist(board,word,count,x+1,y)) return true;
+        if (ifExist(board,word,count,x,y-1)) return true;
+        count--;
         board[x][y] = (char)(temp);
         return false;
     }
