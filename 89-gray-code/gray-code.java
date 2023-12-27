8733a77decb22cb75a6737a0e361d22f382e633b
mryash08 <140238011+mryash08@users.noreply.github.com>
2023-12-27 10:52:32 +0530

    Time: 6 ms (75.44%) | Memory: 50.9 MB (33.18%) - LeetSync

--- 89-gray-code/gray-code.java
+++ 89-gray-code/gray-code.java
@@ -1,22 +1,23 @@
 class Solution {
     public List<Integer> grayCode(int n) {
-        // int[] temp = new int[1 << n];
-        // List<Integer> list = new ArrayList<>();
-        // int curr = 0;
-        // while(list.size() != temp.length){
-        //       list.add(curr);
-        //       temp[curr] = -1;
-        //       int i=0;
-        //       while(i < n){
-        //            int tem = curr ^ (1 << i);
-        //            if(temp[tem] == 0) {
-        //                curr = tem;
-        //                break;
-        //            }
-        //            i++;
-        //       }
-        // }
-        return findGrayCode(n,0,new ArrayList<>(),new boolean[1 << n]);
+        int[] temp = new int[1 << n];
+        List<Integer> list = new ArrayList<>();
+        int curr = 0;
+        while(list.size() != temp.length){
+              list.add(curr);
+              temp[curr] = -1;
+              int i=0;
+              while(i < n){
+                   int tem = curr ^ (1 << i);
+                   if(temp[tem] == 0) {
+                       curr = tem;
+                       break;
+                   }
+                   i++;
+              }
+        }
+        return list;
+        // return findGrayCode(n,0,new ArrayList<>(),new boolean[1 << n]);
     }
     public List<Integer> findGrayCode(int n,int t,List<Integer> ans,boolean[] check){
          if(ans.size() == check.length) return ans;
