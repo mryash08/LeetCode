d57e4a95cb016f4de264401e72fe7c19d830b966
mryash08 <140238011+mryash08@users.noreply.github.com>
2023-12-27 11:59:41 +0530

    Time: 4 ms (98.42%) | Memory: 51.7 MB (27.62%) - LeetSync

--- 89-gray-code/gray-code.java
+++ 89-gray-code/gray-code.java
@@ -1,34 +1,39 @@
 class Solution {
     public List<Integer> grayCode(int n) {
-        int[] temp = new int[1 << n];
         List<Integer> list = new ArrayList<>();
-        int curr = 0;
-        while(list.size() != temp.length){
-              list.add(curr);
-              temp[curr] = -1;
-              int i=0;
-              while(i < n){
-                   int tem = curr ^ (1 << i);
-                   if(temp[tem] == 0) {
-                       curr = tem;
-                       break;
-                   }
-                   i++;
-              }
+        for(int i=0; i<(1<<n); i++){
+            list.add(i ^ (i >> 1));
         }
         return list;
+        // int[] temp = new int[1 << n];
+        // List<Integer> list = new ArrayList<>();
+        // int curr = 0;
+        // while(list.size() != temp.length){
+        //       list.add(curr);
+        //       temp[curr] = -1;
+        //       int i=0;
+        //       while(i < n){
+        //            int tem = curr ^ (1 << i);
+        //            if(temp[tem] == 0) {
+        //                curr = tem;
+        //                break;
+        //            }
+        //            i++;
+        //       }
+        // }
+        // return list;
         // return findGrayCode(n,0,new ArrayList<>(),new boolean[1 << n]);
     }
-    public List<Integer> findGrayCode(int n,int t,List<Integer> ans,boolean[] check){
-         if(ans.size() == check.length) return ans;
-         ans.add(t);
-         check[t] = true;
-         for(int i=0; i<n; i++){
-            int temp = t ^ (1 << i);
-            if(!check[temp]){
-                return findGrayCode(n,temp,ans,check);
-            }
-         }
-         return ans;
-    }
+    // public List<Integer> findGrayCode(int n,int t,List<Integer> ans,boolean[] check){
+    //      if(ans.size() == check.length) return ans;
+    //      ans.add(t);
+    //      check[t] = true;
+    //      for(int i=0; i<n; i++){
+    //         int temp = t ^ (1 << i);
+    //         if(!check[temp]){
+    //             return findGrayCode(n,temp,ans,check);
+    //         }
+    //      }
+    //      return ans;
+    // }
 }
