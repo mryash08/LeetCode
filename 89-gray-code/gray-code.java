ab8d5431e0ef64ecf68da46b25568451c5079731
mryash08 <140238011+mryash08@users.noreply.github.com>
2023-12-27 10:51:26 +0530

    Time: 11 ms (31.14%) | Memory: 62.8 MB (12.05%) - LeetSync

--- 89-gray-code/gray-code.java
+++ 89-gray-code/gray-code.java
@@ -1,21 +1,33 @@
 class Solution {
     public List<Integer> grayCode(int n) {
-        int[] temp = new int[1 << n];
-        List<Integer> list = new ArrayList<>();
-        int curr = 0;
-        while(list.size() != temp.length){
-              list.add(curr);
-              temp[curr] = -1;
-              int i=0;
-              while(i < n){
-                   int tem = curr ^ (1 << i);
-                   if(temp[tem] == 0) {
-                       curr = tem;
-                       break;
-                   }
-                   i++;
-              }
-        }
-        return list;
+        // int[] temp = new int[1 << n];
+        // List<Integer> list = new ArrayList<>();
+        // int curr = 0;
+        // while(list.size() != temp.length){
+        //       list.add(curr);
+        //       temp[curr] = -1;
+        //       int i=0;
+        //       while(i < n){
+        //            int tem = curr ^ (1 << i);
+        //            if(temp[tem] == 0) {
+        //                curr = tem;
+        //                break;
+        //            }
+        //            i++;
+        //       }
+        // }
+        return findGrayCode(n,0,new ArrayList<>(),new boolean[1 << n]);
+    }
+    public List<Integer> findGrayCode(int n,int t,List<Integer> ans,boolean[] check){
+         if(ans.size() == check.length) return ans;
+         ans.add(t);
+         check[t] = true;
+         for(int i=0; i<n; i++){
+            int temp = t ^ (1 << i);
+            if(!check[temp]){
+                return findGrayCode(n,temp,ans,check);
+            }
+         }
+         return ans;
     }
 }
