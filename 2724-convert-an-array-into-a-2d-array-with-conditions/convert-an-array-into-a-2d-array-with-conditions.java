2b935eeb8e5f8acb09773dc533a70e6610e2bc18
mryash08 <140238011+mryash08@users.noreply.github.com>
2024-01-02 09:24:21 +0530

    Time: 2 ms (96.82%) | Memory: 45 MB (9.75%) - LeetSync

--- 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.java
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.java
@@ -1,18 +1,44 @@
 class Solution {
     public List<List<Integer>> findMatrix(int[] nums) {
-        Arrays.sort(nums);
-        List<List<Integer>> ans = new ArrayList<>();
-        int i=0; 
-        while(i<nums.length){
-            int j = i; int index = 0;
-            while(i<nums.length && nums[i] == nums[j]){
-                 if(ans.size() <= index){
+         
+         int[] freq = new int[201];
+         for(int i=0; i<nums.length; i++){
+             freq[nums[i]]++;
+         }
+         List<List<Integer>> ans = new ArrayList<>();
+        for(int i=0; i<freq.length; i++){
+            int j = 0;
+            while(j<freq[i]){
+                 if(ans.size() <= j){
                      ans.add(new ArrayList<>());
                  }
-                 ans.get(index).add(nums[i++]);
-                 index++;
+                 ans.get(j).add(i);
+                 j++;
             }
         }
         return ans;
+
+
+
+
+
+
+
+        // nlogn
+        // Arrays.sort(nums);
+        // List<List<Integer>> ans = new ArrayList<>();
+        // int i=0; 
+        // while(i<nums.length){
+        //     int j = i; int index = 0;
+        //     while(i<nums.length && nums[i] == nums[j]){
+        //          if(ans.size() <= index){
+        //              ans.add(new ArrayList<>());
+        //          }
+        //          ans.get(index).add(nums[i++]);
+        //          index++;
+        //     }
+        // }
+        // return ans;
+
     }
 }
