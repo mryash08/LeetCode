157648322ac49bf9afedcebf4a90f85b998b54e7
mryash08 <140238011+mryash08@users.noreply.github.com>
2024-01-03 10:14:35 +0530

    Time: 17 ms (23.02%) | Memory: 45 MB (20.83%) - LeetSync

--- 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.java
+++ 2244-number-of-laser-beams-in-a-bank/number-of-laser-beams-in-a-bank.java
@@ -1,12 +1,15 @@
 class Solution {
+    public int countBeams(String row){
+        int ans = 0;
+        for(var c : row.toCharArray()){
+             ans += c - '0';
+        }
+        return ans;
+    }
     public int numberOfBeams(String[] bank) {
         int ans = 0; int prev = 0;
         for(int i=0; i<bank.length; i++){
-            int t=0;
-            for(int j=0; j<bank[i].length(); j++){
-                if(bank[i].charAt(j) == '1') t++;
-            }
-            
+            int t= countBeams(bank[i]);
             if(t != 0){
                 ans += prev * t;
                 prev = t;
