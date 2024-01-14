e33b97fb0562963b3b20dec8dcc8c711eab69eb1
mryash08 <140238011+mryash08@users.noreply.github.com>
2024-01-14 21:12:44 +0530

    Time: 10 ms (92.33%) | Memory: 45.3 MB (40.42%) - LeetSync

--- 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
+++ 1777-determine-if-two-strings-are-close/determine-if-two-strings-are-close.java
@@ -1,14 +1,12 @@
 class Solution {
     private int N = 26;
     public boolean closeStrings(String word1, String word2) {
-		// count the English letters
         int[] arr1 = new int[N], arr2 = new int[N];
         for (char ch : word1.toCharArray())
             arr1[ch - 'a']++;
         for (char ch : word2.toCharArray())
             arr2[ch - 'a']++;
-		
-		// if one has a letter which another one doesn't have, dont exist
+
         for (int i = 0; i < N; i++) {
             if (arr1[i] == arr2[i]) {
                 continue;
