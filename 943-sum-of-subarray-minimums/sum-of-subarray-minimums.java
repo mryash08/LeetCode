ee978e347cc9bf75c558fda2fcc2b231367e1e98
mryash08 <140238011+mryash08@users.noreply.github.com>
2024-01-20 15:27:03 +0530

    Time: 359 ms (5.09%) | Memory: 50.1 MB (30.77%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
@@ -1,20 +1,20 @@
 class Solution {
     public int sumSubarrayMins(int[] arr) {
-        long sum = 0;
-        for(int i=0; i<arr.length; i++){
-            long left = 1; int j = i+1;
-            long right = 1;
-            while( j < arr.length && arr[i] <= arr[j]){
-                j++;
-                right++;
+        long res = arr[0];
+        long[] sum = new long[arr.length];
+        sum[0] = arr[0];
+        for(int i=1; i<arr.length; i++){
+            int left = i-1; long count = 1;
+            while(left >= 0 && arr[left] > arr[i]){
+                count++; left--;
             }
-            j = i-1;
-            while( j >= 0 && arr[i] < arr[j]){
-                j--;
-                left++;
+            if(left == -1){
+               sum[i] = (arr[i]*count);
+            }else{
+                sum[i] = sum[left] + (arr[i]*count);
             }
-            sum += left * right * arr[i];
+            res += sum[i];
         }
-        return (int)(sum % 1000000007);
+        return (int)(res % 1000000007);
     }
 }
