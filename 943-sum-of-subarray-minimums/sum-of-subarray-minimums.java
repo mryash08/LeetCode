b930bf0fd26c838a500ccd0863769d57e4f0b035
mryash08 <140238011+mryash08@users.noreply.github.com>
2024-01-20 15:28:17 +0530

    Time: 5 ms (99.40%) | Memory: 50 MB (31.27%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
@@ -1,20 +1,24 @@
 class Solution {
+    static int mod =(int) 1e9 +7;
     public int sumSubarrayMins(int[] arr) {
-        long res = arr[0];
-        long[] sum = new long[arr.length];
-        sum[0] = arr[0];
-        for(int i=1; i<arr.length; i++){
-            int left = i-1; long count = 1;
-            while(left >= 0 && arr[left] > arr[i]){
-                count++; left--;
-            }
-            if(left == -1){
-               sum[i] = (arr[i]*count);
-            }else{
-                sum[i] = sum[left] + (arr[i]*count);
-            }
-            res += sum[i];
+        int n = arr.length + 1;
+        int[] left = new int[n];
+        int[] ext = new int[n];
+        int[] sums = new int[n];
+        for(int i = 0; i < n -1; ++i)
+           ext[i+1] = arr[i];
+           
+        int res = 0;
+        for (int i = 1; i < n; i++) {
+            int cur = ext[i];
+            int l = i - 1;
+            while (ext[l] >= cur) 
+                l = left[l];
+            
+            left[i] = l;
+            sums[i] = sums[l] + cur * (i - l);
+            res = (res + sums[i]) % mod;
         }
-        return (int)(res % 1000000007);
+        return res;        
     }
 }
