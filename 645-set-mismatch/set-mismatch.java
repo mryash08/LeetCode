a3527153b852c16c06043fd9c70084c8e5e80d77
mryash08 <140238011+mryash08@users.noreply.github.com>
2024-01-22 10:06:20 +0530

    Time: 1 ms (100.00%) | Memory: 45.3 MB (52.40%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -1,24 +1,24 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
+     int x[] = new int[nums.length+1];
 
-        for(int i=0; i<nums.length; i++){
-            while(i+1 != nums[i] && nums[nums[i]-1] != nums[i]){
-                swap(nums, nums[i]-1,i);
-            }
-        }
-        int[] ans = new int[2];
-        for(int i=0; i<nums.length; i++){
-            if(i+1 != nums[i]){
-                ans[0] = nums[i];
-                ans[1] = i+1;
-                return ans;
-            }
-        }
-       return ans;
+     for(int i:nums){
+         x[i]++;
+     }
+
+     int dup=0 , miss=0;
+
+     for(int i=1;i<x.length;i++){
+         if(x[i]==2){
+             dup=i;
+         }
+         if(x[i] ==0){
+             miss=i;
+         }
+     }
+
+return new int[]{dup,miss};
+      
+        
     }
-    public void swap(int[] nums,int i,int j){
-        int temp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = temp;
-    } 
 }
