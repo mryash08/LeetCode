0832edbd36ed47435d0d9d4f8d1163184ee47c9b
mryash08 <140238011+mryash08@users.noreply.github.com>
2024-01-22 10:06:55 +0530

    Time: 2 ms (97.74%) | Memory: 45.5 MB (31.90%) - LeetSync

--- 645-set-mismatch/set-mismatch.java
+++ 645-set-mismatch/set-mismatch.java
@@ -1,24 +1,20 @@
 class Solution {
     public int[] findErrorNums(int[] nums) {
-     int x[] = new int[nums.length+1];
-
+     int freq[] = new int[nums.length+1];
      for(int i:nums){
-         x[i]++;
+         freq[i]++;
      }
 
      int dup=0 , miss=0;
 
-     for(int i=1;i<x.length;i++){
-         if(x[i]==2){
+     for(int i=1;i<freq.length;i++){
+         if(freq[i]==2){
              dup=i;
          }
-         if(x[i] ==0){
+         if(freq[i] ==0){
              miss=i;
          }
      }
-
-return new int[]{dup,miss};
-      
-        
+           return new int[]{dup,miss};
     }
 }
