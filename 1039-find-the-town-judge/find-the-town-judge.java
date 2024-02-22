9071ddd25aa530633489a55b923a3e50fca6a1ae
mryash08 <140238011+mryash08@users.noreply.github.com>
2024-02-22 10:00:58 +0530

    Time: 2 ms (99.85%) | Memory: 54.2 MB (21.53%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.java
+++ 1039-find-the-town-judge/find-the-town-judge.java
@@ -1,15 +1,13 @@
 class Solution {
     public int findJudge(int n, int[][] trust) {
-        for(int i=1; i<=n; i++){
-            int tru = 0;
-            for(int j=0; j<trust.length; j++){
-                if(trust[j][1] == i){
-                    tru++;
-                }
-            }
-            if(tru >= n-1){
+        int[] freq = new int[n+1];
+        for(int i=0; i<trust.length; i++){
+                freq[trust[i][1]]++;
+        }
+        for(int i=1; i<freq.length; i++){
+            if(freq[i] >= n-1){ 
                 boolean flag = true;
-                for(int j=0; j<trust.length; j++){
+                for(int j = 0; j<trust.length; j++){
                     if(trust[j][0] == i && trust[j][1] != i){
                         flag = false;
                     }
