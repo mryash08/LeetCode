75e6bd2a64ceb70f7f587759b9d2d27ea09f6a3e
mryash08 <140238011+mryash08@users.noreply.github.com>
2024-03-21 09:23:39 +0530

    Time: 3 ms (86.83%) | Memory: 49.5 MB (43.27%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,11 +1,10 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        boolean[][] isVisited = new boolean[grid.length][grid[0].length];
         int ans = 0;
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
-                if (!isVisited[i][j] && grid[i][j] == '1') {
-                    Helper(grid, i, j, isVisited);
+                if (grid[i][j] == '1') {
+                    Helper(grid, i, j);
                     ans++;
                 }
             }
@@ -14,14 +13,14 @@ class Solution {
 
     }
 
-    public void Helper(char[][] grid, int i, int j, boolean[][] isVisited) {
-        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || isVisited[i][j] || grid[i][j] == '0') {
+    public void Helper(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length ||  grid[i][j] == '0') {
             return;
         }
-        isVisited[i][j] = true;
-        Helper(grid, i - 1, j, isVisited);
-        Helper(grid, i, j - 1, isVisited);
-        Helper(grid, i + 1, j, isVisited);
-        Helper(grid, i, j + 1, isVisited);
+        grid[i][j] = '0';
+        Helper(grid, i - 1, j);
+        Helper(grid, i, j - 1);
+        Helper(grid, i + 1, j);
+        Helper(grid, i, j + 1);
     }
 }
