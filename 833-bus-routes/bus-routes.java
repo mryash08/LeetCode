d10bfd8f9fedbed57ec0c722a7f78130f5214e2c
mryash08 <140238011+mryash08@users.noreply.github.com>
2024-03-26 17:52:10 +0530

    Time: 4 ms (99.48%) | Memory: 56.2 MB (93.61%) - LeetSync

--- 833-bus-routes/bus-routes.java
+++ 833-bus-routes/bus-routes.java
@@ -1,45 +1,77 @@
 class Solution {
-    public int numBusesToDestination(int[][] routes, int source, int target) {
-         if(source == target) return 0;
-         HashMap<Integer, List<Integer>> map = new HashMap<>();
-         for(int i=0; i<routes.length; i++){
-            for(int j=0; j<routes[i].length; j++){
-                if(!map.containsKey(routes[i][j])){
-                    map.put(routes[i][j],new ArrayList<>());
-                }
-                List<Integer> list = map.get(routes[i][j]);
-                list.add(i);
-                map.put(routes[i][j],list);
+    public int numBusesToDestination(int[][] routes, int source, int target){
+        int maxStation = 0;
+        for(int[] i : routes){
+            for(int j : i){
+                maxStation = Math.max(maxStation,j);
             }
-         }
-        
-         if(!map.containsKey(source)) return -1;
-         if(!map.containsKey(target)) return -1;
-        HashSet<Integer> bus = new HashSet<>();
-        HashSet<Integer> busStation = new HashSet<>();
-        Queue<Integer> queue = new ArrayDeque<>();
-        int level = 0;
-        queue.add(source);
-        busStation.add(source);
-        while(queue.size() != 0){
-            int size = queue.size();
-            while(size-- > 0){
-                int temp = queue.remove();
-                if(temp == target) return level;
-                busStation.add(temp);
-                for(int i=0; i<map.get(temp).size(); i++){
-                    if(!bus.contains(map.get(temp).get(i))){
-                        bus.add(map.get(temp).get(i));
-                        for(int j=0; j<routes[map.get(temp).get(i)].length; j++){
-                            if(!busStation.contains(routes[map.get(temp).get(i)][j])){
-                                queue.add(routes[map.get(temp).get(i)][j]);
-                            }
-                        }
+        }
+        if(source == target) return 0;
+        if(target > maxStation || source > maxStation) return -1;
+        int[] dp = new int[maxStation+1];
+        Arrays.fill(dp,routes.length+1);
+        boolean flag = true;
+        dp[source] = 0;
+        while(flag){
+            flag = false;
+            for(int[] i : routes){
+                int min = routes.length+1;
+                for(int j : i){
+                     min = Math.min(dp[j],min);
+                }
+                min++;
+                for(int j : i){
+                    if(dp[j] > min){
+                        dp[j] = min;
+                        flag = true;
                     }
                 }
             }
-            level++;
         }
-        return -1;
+        if(dp[target] == routes.length+1) return -1;
+        return dp[target];
     }
+    // public int numBusesToDestination(int[][] routes, int source, int target) {
+    //      if(source == target) return 0;
+    //      HashMap<Integer, List<Integer>> map = new HashMap<>();
+    //      for(int i=0; i<routes.length; i++){
+    //         for(int j=0; j<routes[i].length; j++){
+    //             if(!map.containsKey(routes[i][j])){
+    //                 map.put(routes[i][j],new ArrayList<>());
+    //             }
+    //             List<Integer> list = map.get(routes[i][j]);
+    //             list.add(i);
+    //             map.put(routes[i][j],list);
+    //         }
+    //      }
+        
+    //      if(!map.containsKey(source)) return -1;
+    //      if(!map.containsKey(target)) return -1;
+    //     HashSet<Integer> bus = new HashSet<>();
+    //     HashSet<Integer> busStation = new HashSet<>();
+    //     Queue<Integer> queue = new ArrayDeque<>();
+    //     int level = 0;
+    //     queue.add(source);
+    //     busStation.add(source);
+    //     while(queue.size() != 0){
+    //         int size = queue.size();
+    //         while(size-- > 0){
+    //             int temp = queue.remove();
+    //             if(temp == target) return level;
+    //             busStation.add(temp);
+    //             for(int i=0; i<map.get(temp).size(); i++){
+    //                 if(!bus.contains(map.get(temp).get(i))){
+    //                     bus.add(map.get(temp).get(i));
+    //                     for(int j=0; j<routes[map.get(temp).get(i)].length; j++){
+    //                         if(!busStation.contains(routes[map.get(temp).get(i)][j])){
+    //                             queue.add(routes[map.get(temp).get(i)][j]);
+    //                         }
+    //                     }
+    //                 }
+    //             }
+    //         }
+    //         level++;
+    //     }
+    //     return -1;
+    // }
 }
