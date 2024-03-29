33dc8408d5579331affcb6b92954ef55a1a8f084
mryash08 <140238011+mryash08@users.noreply.github.com>
2024-03-29 14:35:39 +0530

    Time: 12 ms (99.46%) | Memory: 64 MB (48.37%) - LeetSync

--- 2832-find-the-longest-equal-subarray/find-the-longest-equal-subarray.java
+++ 2832-find-the-longest-equal-subarray/find-the-longest-equal-subarray.java
@@ -1,28 +1,39 @@
 class Solution {
     public int longestEqualSubarray(List<Integer> nums, int k) {
-        HashMap<Integer,List<Integer>> map = new HashMap<>();
-        for(int i=0; i<nums.size(); i++){
-             if(!map.containsKey(nums.get(i))){
-                 map.put(nums.get(i),new ArrayList<>());
-             }
-             List<Integer> list = map.get(nums.get(i));
-             list.add(i);
-        }
-         int ans = 1;
-        for(int i : map.keySet()){
-            int k1 = k;
-            int left = 0; int right = 0;
-            List<Integer> list = map.get(i);
-            while(right < list.size()-1){
-                k1 -= list.get(right+1) - list.get(right)-1;
-                right++;
-                while(left <= right && k1 < 0){
-                    k1 += list.get(left+1) - list.get(left) -1;
-                    left++;
-                }
-                 ans = Math.max(ans,right-left+1);
+        int right = 0; int left = 0; int ans = 0;
+        int[] freq = new int[nums.size()+1];
+        while(right < nums.size()){
+            if(right - left - ans > k){
+                freq[nums.get(left)]--;
+                left++;
             }
+            ans = Math.max(ans,++freq[nums.get(right)]);
+            right++;
         }
         return ans;
+        // HashMap<Integer,List<Integer>> map = new HashMap<>();
+        // for(int i=0; i<nums.size(); i++){
+        //      if(!map.containsKey(nums.get(i))){
+        //          map.put(nums.get(i),new ArrayList<>());
+        //      }
+        //      List<Integer> list = map.get(nums.get(i));
+        //      list.add(i);
+        // }
+        //  int ans = 1;
+        // for(int i : map.keySet()){
+        //     int k1 = k;
+        //     int left = 0; int right = 0;
+        //     List<Integer> list = map.get(i);
+        //     while(right < list.size()-1){
+        //         k1 -= list.get(right+1) - list.get(right)-1;
+        //         right++;
+        //         while(left <= right && k1 < 0){
+        //             k1 += list.get(left+1) - list.get(left) -1;
+        //             left++;
+        //         }
+        //          ans = Math.max(ans,right-left+1);
+        //     }
+        // }
+        // return ans;
     }
 }
