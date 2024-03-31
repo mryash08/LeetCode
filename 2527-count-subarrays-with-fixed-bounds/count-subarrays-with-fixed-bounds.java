84f68824c050311573bb7b2e03b833849adfae80
mryash08 <140238011+mryash08@users.noreply.github.com>
2024-03-31 08:45:51 +0530

    Time: 8 ms (51.62%) | Memory: 61.6 MB (7.58%) - LeetSync

--- 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.java
+++ 2527-count-subarrays-with-fixed-bounds/count-subarrays-with-fixed-bounds.java
@@ -1,12 +1,12 @@
 class Solution {
         public long countSubarrays(int[] A, int minK, int maxK) {
-        long res = 0, jbad = -1, jmin = -1, jmax = -1, n = A.length;
+        long ans = 0, oRange = -1, min = -1, max = -1, n = A.length;
         for (int i = 0; i < n; i++) {
-            if (A[i] < minK || A[i] > maxK) jbad = i;
-            if (A[i] == minK) jmin = i;
-            if (A[i] == maxK) jmax = i;
-            res += Math.max(0L, Math.min(jmin, jmax) - jbad);
+            if (A[i] < minK || A[i] > maxK) oRange = i;
+            if (A[i] == minK) min = i;
+            if (A[i] == maxK) max = i;
+            ans += Math.max(0L, Math.min(min, max) - oRange);
         }
-        return res;
+        return ans;
     }
 }
