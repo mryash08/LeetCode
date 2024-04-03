5de23898c8f3cad59faecdde6196c44ca6ca627e
mryash08 <140238011+mryash08@users.noreply.github.com>
2024-04-03 08:55:13 +0530

    Time: 104 ms (93.54%) | Memory: 41.6 MB (39.67%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,23 +1,24 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
+        char[] wordA = word.toCharArray();
         for(int i=0; i<board.length; i++){
             for(int j=0; j<board[0].length; j++){
-                if(ifExist(board,word,0,i,j)) return true;
+                if(ifExist(board,wordA,0,i,j)) return true;
             }
         }
         return false;
     }
-    public boolean ifExist(char[][] board,String word,int count,int x,int y){
-        if(count == word.length()) return true;
+    public boolean ifExist(char[][] board,char[] wordA,int count,int x,int y){
+        if(count == wordA.length) return true;
         if(x < 0 || y < 0 || x >= board.length || y >= board[0].length) return false;
-        if(word.charAt(count) != board[x][y]) return false;
+        if(wordA[count] != board[x][y]) return false;
         int temp = board[x][y];
         board[x][y] = ' ';
         count++;
-        if(ifExist(board,word,count,x-1,y)) return true;
-        if(ifExist(board,word,count,x,y+1)) return true;
-        if(ifExist(board,word,count,x+1,y)) return true;
-        if (ifExist(board,word,count,x,y-1)) return true;
+        if(ifExist(board,wordA,count,x-1,y)) return true;
+        if(ifExist(board,wordA,count,x,y+1)) return true;
+        if(ifExist(board,wordA,count,x+1,y)) return true;
+        if (ifExist(board,wordA,count,x,y-1)) return true;
         count--;
         board[x][y] = (char)(temp);
         return false;
