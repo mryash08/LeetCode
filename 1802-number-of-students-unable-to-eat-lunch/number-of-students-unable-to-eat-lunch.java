c8005f39ab21d7145e5014946f89299a9608cd35
mryash08 <140238011+mryash08@users.noreply.github.com>
2024-04-08 14:44:20 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (95.65%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.java
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.java
@@ -1,48 +1,20 @@
 class Solution {
     public int countStudents(int[] students, int[] sandwiches) {
-        Queue<Integer> queue = new LinkedList<>();
-        Stack<Integer> stack = new Stack<>();
         int zero = 0;
         int one = 0;
-
         for(int i=0; i<students.length; i++){
              if(students[i] == 0){
                  zero++;
              }else{
                  one++;
              }
-             queue.add(students[i]);
         }
-
-        for(int i=sandwiches.length-1; i>=0; i--){
-            stack.push(sandwiches[i]);
+        for(int i : sandwiches){
+            if(i == 0 && zero != 0) zero--;
+            else if(i == 0  && zero == 0) return one;
+            else if(i == 1 && one != 0) one--;
+            else return zero; 
         }
-
-        while(queue.size()!=0){
-
-             if(stack.peek() == 0 && zero == 0){
-                return queue.size();
-            }
-            if(stack.peek() == 1 && one == 0){
-                return queue.size();
-            }
-            if(queue.peek() == stack.peek()){
-
-                int temp = queue.remove();
-                if(temp == 1){
-                    one--;
-                }else{
-                    zero--;
-                }
-
-                stack.pop();
-            }else{
-                queue.add(queue.remove());
-            }
-
-           
-        }
-
-        return queue.size();
+        return zero + one;
     }
 }
