b24735042184f42aa95747ab41afebe10b909f2b
mryash08 <140238011+mryash08@users.noreply.github.com>
2024-04-15 11:10:34 +0530

    Time: 5 ms (98.38%) | Memory: 43.4 MB (29.09%) - LeetSync

--- 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.java
+++ 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.java
@@ -1,12 +1,14 @@
 class Solution {
     public int equalSubstring(String s, String t, int maxCost) {
+        char[] sArr = s.toCharArray();
+        char[] tArr = t.toCharArray();
         int ans = 0;
         int start = 0;
         int end = 0;
-        while(end < s.length()){
-            maxCost -= Math.abs(t.charAt(end) - s.charAt(end));
+        while(end < sArr.length){
+            maxCost -= Math.abs(sArr[end] - tArr[end]);
             while(maxCost < 0){
-                maxCost += Math.abs(t.charAt(start) - s.charAt(start));
+                maxCost += Math.abs(sArr[start] - tArr[start]);
                 start++;
             }
             ans = Math.max(ans,end - start +1);
