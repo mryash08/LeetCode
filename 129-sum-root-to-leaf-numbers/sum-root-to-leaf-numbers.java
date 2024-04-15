d17724d8aff71fd2a5c6e7e7aed3b78cd58f0eb0
mryash08 <140238011+mryash08@users.noreply.github.com>
2024-04-15 09:25:27 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (86.84%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
@@ -1,40 +1,56 @@
 /**
  * Definition for a binary tree node.
  * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
+ * int val;
+ * TreeNode left;
+ * TreeNode right;
+ * TreeNode() {}
+ * TreeNode(int val) { this.val = val; }
+ * TreeNode(int val, TreeNode left, TreeNode right) {
+ * this.val = val;
+ * this.left = left;
+ * this.right = right;
+ * }
  * }
  */
 class Solution {
+    int sum = 0;
+
     public int sumNumbers(TreeNode root) {
-        Stack<TreeNode> stack = new Stack<>();
-        int ans = 0;
-        int num = 0;
-        TreeNode curr = root;
-        TreeNode prev = null;
-        while(curr != null || !stack.isEmpty()){
-            while(curr != null){
-                stack.push(curr);
-                num = num * 10 + curr.val;
-                curr = curr.left;
-            }
-            curr = stack.peek();
-            if(curr.right != null && curr.right != prev) curr = curr.right;
-            else{ 
-                if(curr.left == null && curr.right == null) ans += num;
-                 num /= 10;
-                 prev = stack.pop();
-                 curr = null;
-            } 
-        }
-        return ans;
+        return sum(root, 0);
     }
+
+    public int sum(TreeNode root, int sum) {
+        if (root == null)
+            return 0;
+        if (root.right == null && root.left == null)
+            return sum * 10 + root.val;
+        return sum(root.left, sum * 10 + root.val) + sum(root.right, sum * 10 + root.val);
+    }
+
+    // public int sumNumbers(TreeNode root) {
+    //     Stack<TreeNode> stack = new Stack<>();
+    //     int ans = 0;
+    //     int num = 0;
+    //     TreeNode curr = root;
+    //     TreeNode prev = null;
+    //     while (curr != null || !stack.isEmpty()) {
+    //         while (curr != null) {
+    //             stack.push(curr);
+    //             num = num * 10 + curr.val;
+    //             curr = curr.left;
+    //         }
+    //         curr = stack.peek();
+    //         if (curr.right != null && curr.right != prev)
+    //             curr = curr.right;
+    //         else {
+    //             if (curr.left == null && curr.right == null)
+    //                 ans += num;
+    //             num /= 10;
+    //             prev = stack.pop();
+    //             curr = null;
+    //         }
+    //     }
+    //     return ans;
+    // }
 }
