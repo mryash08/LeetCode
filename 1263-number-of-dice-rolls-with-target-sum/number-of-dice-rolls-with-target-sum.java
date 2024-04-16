7cefd476ca7e6282bb082d0e30cdaab0766f4276
mryash08 <140238011+mryash08@users.noreply.github.com>
2024-04-16 10:06:08 +0530

    Time: 23 ms (31.64%) | Memory: 41.7 MB (73.45%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.java
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.java
@@ -5,7 +5,7 @@ class Solution {
         for(int i=1; i<dp.length; i++){
             for(int j=1; j<dp[0].length; j++){
                 for(int a=1; a<=k; a++){
-                    if(j >= a) dp[i][j] = ((dp[i][j]%1000000007) + (dp[i-1][j-a]%1000000007))%1000000007;
+                    if(j >= a) dp[i][j] = ((dp[i][j]) + (dp[i-1][j-a]))%1000000007;
                 }
             }
         }
